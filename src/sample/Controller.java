85aa091e566d44be46ac5613502d2f4522c9dac9
Levente Ibrányi <ibranyilevente@gmail.com>
2021-12-09 11:53:01 +0100

    Buttons disabled

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button btnLegDraga;
+    @FXML
+    private Button btnOsszes;
+    @FXML
+    private Button btnDraga;
+    @FXML
+    private Button btnAkcios;
+    @FXML
+    private Button btnFajl;
+    @FXML
+    private Button btnHibas;
+    @FXML
+    private Button btnKilep;
+
+    public void initialize(){
+        btnLegDraga.setDisable(true);
+        btnOsszes.setDisable(true);
+        btnDraga.setDisable(true);
+        btnAkcios.setDisable(true);
+        btnHibas.setDisable(true);
+    }
 }
